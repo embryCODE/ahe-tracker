a925ff85462d0b7cf230333ad00236984fef7daa
Mason Embry <Mason.Embry@hcahealthcare.com>
2017-05-16 13:23:39 -0500

    Troubleshoot heroku deployment

--- server/server.js
+++ server/server.js
@@ -30,6 +30,6 @@ restify.serve(router, Food)
 app.use('/', express.static(path.join(__dirname, '../public')))
 app.use(router)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Express server listening on port 3000')
 })
