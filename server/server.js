ee1d840f92892c722bface12e28d33a5c28a5296
Mason Embry <Mason.Embry@hcahealthcare.com>
2017-05-17 12:20:14 -0500

    Make count endpoint more readable

--- server/server.js
+++ server/server.js
@@ -3,9 +3,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const controller = require('./controller')
+const mongoose = require('mongoose')
 
 // Constants
 const port = process.env.PORT || 3000
+const dbUrl = process.env.DB_URL || require('../config').dbUrl
+
+// Set Mongoose to use native promises
+mongoose.Promise = global.Promise
+
+// Connect to mLab Mongo database
+mongoose.connect(dbUrl)
 
 // Setup Express
 const app = express()
@@ -14,11 +22,11 @@ const app = express()
 app.use(bodyParser.json())
 
 // Routing
-app.get('/', express.static(path.join(__dirname, '../public')))
 app.get('/api/users', controller.getAllUsers)
 app.get('/api/users/:id', controller.getUserById)
 app.post('/api/users', controller.createUser)
-app.put('/api/users/:id/:foodId/:foodCount', controller.updateFoodCount)
+app.put('/api/users/:id/foods/:foodId/count/:foodCount', controller.updateFoodCount)
+app.get('/*', express.static(path.join(__dirname, '../public')))
 
 // Start server
 app.listen(port, () => {
